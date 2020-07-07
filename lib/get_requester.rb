7ed51313ae9baaaec71daae7cde5e4bc230f6c3a
Sara Amiry <saraamiry24@gmail.com>
2020-07-07 03:52:32 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,23 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+  URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+  def get_response_body
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end 
+  
+  def parse_json
+    program = JSON.parse(self.get_requester)
+    program.collect do |requests| 
+      requests["agency"]
+    end 
+  end 
+end 
+programs = GetRequester.new 
+puts programs.parse_json.uniq 
+#requester = GetRequester.new.get_requester
+#puts requester
